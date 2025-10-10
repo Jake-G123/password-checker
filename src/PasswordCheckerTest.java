97a0674f3166141397124da01f30243ce3850db6
Jonus <joclapshaw@gmail.com>
2025-10-10 10:47:00 -0700

    Changes cause of errors for structure

--- src/PasswordCheckerTest.java
+++ src/PasswordCheckerTest.java
@@ -4,24 +4,25 @@ import org.junit.jupiter.api.Test;
 
 public class PasswordCheckerTest {
 
-@Test
-    public void testExitInPassword() {
-        // Arrange
-        PasswordChecker testExit = new PasswordChecker(6, 12);
-        // act
-        String actual = testExit.describePasswordLength("exit123");
-        // Assert
-        assertEquals("medium", actual);
-    }
+  @Test
+  public void testExitInPassword() {
+      // Arrange
+      PasswordChecker testExit = new PasswordChecker(6, 12);
+      // act
+      String actual = testExit.describePasswordLength("exit123");
+      // Assert
+      assertEquals("medium", actual);
+  }
 
-    public void textValidExit()
-    {
-        // Arrange
-        PasswordChecker testExit = new PasswordChecker(6, 12);
-        // act
-        boolean actual = testExit.isBannedPassword("exit1");
-        // Assert
-        assertEquals(false, actual);
-    } 
+@Test
+  public void textValidExit()
+  {
+      // Arrange
+      PasswordChecker testExit = new PasswordChecker(6, 12);
+      // act
+      boolean actual = testExit.isBannedPassword("exit1");
+      // Assert
+      assertEquals(false, actual);
+  } 
 
 }
